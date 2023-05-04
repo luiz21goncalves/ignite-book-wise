53cfe17a47a2efe418c323a3f0dd42aa03e1d8eb
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-05-04 17:05:19 -0300

    chore: add next-auth routes

---
+++ src/app/api/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+
+import { prisma } from '@/lib/prisma'
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+})
+
+export { handler as GET, handler as POST }
