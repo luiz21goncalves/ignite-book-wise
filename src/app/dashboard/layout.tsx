caa7203b8c808a09f79a033e85634287cae9f01c
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-05-09 07:52:38 -0300

    build: ignore types for async component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,6 +9,7 @@ type DashboardLayoutProps = {
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
     <div className="w-full h-full flex overflow-auto">
+      {/* @ts-ignore Async component */}
       <Sidebar />
 
       <div className="w-full max-w-[1188px] h-full px-24 pt-[4.5rem] mx-auto">
