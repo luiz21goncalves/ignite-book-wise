bb8c56fbc292328557eebde0026bb9a58125058e
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-06-27 20:44:23 -0300

    feat: add nunito font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Nunito } from 'next/font/google'
+
+const nunito = Nunito({ subsets: ['latin'], variable: '--default' })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={nunito.variable}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
