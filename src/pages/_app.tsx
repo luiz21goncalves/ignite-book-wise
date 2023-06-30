29c9c8561580231ce854018bcb071c85eec3b3a6
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-06-29 21:19:04 -0300

    chore: add app colos to tailwind config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app'
 import { Nunito } from 'next/font/google'
 
-const nunito = Nunito({ subsets: ['latin'], variable: '--default' })
+import '@/styles/global.css'
+
+const nunito = Nunito({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={nunito.variable}>
-      <Component {...pageProps} />
+    <div className={nunito.className}>
+      <div className="w-screen h-screen bg-gray-800 text-gray-100 flex items-center justify-center">
+        <Component {...pageProps} />
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['src/pages/**/*.tsx', 'src/components/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Nunito, sans-serif',
+      },
+      colors: {
+        white: '#ffffff',
+        black: '#000000',
+
+        purple: {
+          100: '#8381D9',
+          200: '#2A2879',
+        },
+
+        gray: {
+          100: '#F8F9FC',
+          200: '#E6E8F2',
+          300: '#D1D6E4',
+          400: '#8D95AF',
+          500: '#303F73',
+          600: '#252D4A',
+          700: '#181C2A',
+          800: '#0E1116',
+        },
+      },
+      borderRadius: {
+        xs: '0.15625rem',
+        sm: '0.3125rem',
+        md: '0.625rem',
+        lg: '1.25rem',
+      },
+      backgroundImage: {
+        'gradient-vertical': `linear-gradient(180deg, #7FD1CC 0%, #9694F5 100%)`,
+        'gradient-horizontal': `linear-gradient(90deg, #7FD1CC 0%, #9694F5 100%)`,
+      },
+    },
   },
   plugins: [],
 }
