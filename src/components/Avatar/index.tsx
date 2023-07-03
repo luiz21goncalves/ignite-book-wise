19bde53a9b4351ae41c75b7d2064960ce98e1829
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-07-03 19:48:38 -0300

    feat: add border to avatar component

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 
 export function Avatar() {
   return (
-    <Image
-      src="https://github.com/diego3g.png"
-      alt=""
-      width={36}
-      height={36}
-      className="h-9 w-9 rounded-full"
-    />
+    <div className="h-fit w-fit overflow-hidden rounded-full bg-gradient-vertical p-[1px]">
+      <Image
+        src="https://github.com/diego3g.png"
+        alt=""
+        width={36}
+        height={36}
+        className="h-9 w-9 rounded-full"
+      />
+    </div>
   )
 }
