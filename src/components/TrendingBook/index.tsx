e96753974a3a50ce42fd4f26e9445511b994dccf
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-07-08 10:21:22 -0300

    feat: add read tag on trending book component

--- src/components/TrendingBook/index.tsx
+++ src/components/TrendingBook/index.tsx
@@ -2,9 +2,24 @@ import Image from 'next/image'
 
 import { Rating } from '../Rating'
 
-export function TrendingBook() {
+type TrendingBookProps = {
+  isRead?: boolean
+  size?: 'md' | 'sm'
+}
+
+export function TrendingBook(props: TrendingBookProps) {
+  const { isRead = false, size = 'md' } = props
+
   return (
-    <div className="flex gap-5 rounded-lg border-2 border-transparent bg-gray-700 px-5 py-4 transition-colors hover:border-gray-600">
+    <div
+      className="relative flex gap-5 overflow-hidden rounded-lg border-2 border-transparent bg-gray-700 px-5 py-4 transition-colors hover:border-gray-600"
+      data-size={size}
+    >
+      {isRead && (
+        <span className="absolute right-0 top-0 rounded-bl bg-green-300 px-2 py-[0.125rem] text-xs font-bold uppercase text-green-100">
+          Lido
+        </span>
+      )}
       <Image
         src={''}
         alt=""
