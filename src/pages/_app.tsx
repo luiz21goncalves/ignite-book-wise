2b6f2607c056ab9c3d5a6bad4f5c83dd542503a3
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-07-09 09:49:27 -0300

    chore: add react query proviver

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,20 @@ import type { AppProps } from 'next/app'
 import { Nunito } from 'next/font/google'
 
 import '@/styles/global.css'
+import { QueryClientProvider } from '@tanstack/react-query'
+
+import { queryClient } from '@/lib/query-client'
 
 const nunito = Nunito({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={nunito.className}>
-      <div className="flex h-screen w-screen items-center justify-center bg-gray-800 text-gray-100">
-        <Component {...pageProps} />
+    <QueryClientProvider client={queryClient}>
+      <div className={nunito.className}>
+        <div className="flex h-screen w-screen items-center justify-center bg-gray-800 text-gray-100">
+          <Component {...pageProps} />
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   )
 }
