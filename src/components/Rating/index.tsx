a4cd22b96d278924a1ed2c610d26bd8e202015cf
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-07-22 09:37:01 -0300

    feat: add tooltip in rating component

--- src/components/Rating/index.tsx
+++ src/components/Rating/index.tsx
@@ -1,23 +1,87 @@
-import { Star } from '@phosphor-icons/react'
+import { Star, StarHalf, IconProps, Icon } from '@phosphor-icons/react'
+import * as Tooltip from '@radix-ui/react-tooltip'
 
 type RatingProps = {
   rate: number
 }
 
-const starts = Array.from({ length: 5 })
+type StarProps = {
+  icon: Icon
+  props: IconProps
+  id: number
+}
+
+const MAX_STAR_RATING = 5
 
 export function Rating(props: RatingProps) {
   const { rate } = props
 
+  const starts = makeStartArray(rate)
+
   return (
-    <div className="flex gap-1 text-purple-100">
-      {starts.map((_, index) => {
-        const isActive = index + 1 <= rate
+    <Tooltip.Provider>
+      <Tooltip.Root>
+        <Tooltip.Trigger className="flex w-fit gap-1 text-purple-100">
+          {starts.map((star) => {
+            const Icon = star.icon
+            return <Icon key={star.id} {...star.props} />
+          })}
+        </Tooltip.Trigger>
 
-        const weight = isActive ? 'fill' : 'bold'
-
-        return <Star key={index} weight={weight} />
-      })}
-    </div>
+        <Tooltip.Portal>
+          <Tooltip.Content
+            className="rounded bg-purple-100 px-4 py-1 text-button-sm text-gray-200 data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade"
+            sideOffset={4}
+          >
+            {rate} Estrelas
+            <Tooltip.Arrow className="fill-purple-100" />
+          </Tooltip.Content>
+        </Tooltip.Portal>
+      </Tooltip.Root>
+    </Tooltip.Provider>
   )
+}
+
+function makeStartArray(rate: number): StarProps[] {
+  const starts = Array.from({ length: MAX_STAR_RATING }).map<StarProps>(
+    (_, index) => {
+      const truncatedRate = Number(rate.toFixed(1))
+      const position = index + 1
+      const difference = Number((truncatedRate - position).toFixed(1))
+      const isCompleted = difference > 0 || difference >= -0.6
+      const isHalf = difference <= -0.4 && difference >= -0.6
+
+      console.log({ truncatedRate, position, difference })
+
+      if (isHalf) {
+        return {
+          id: position,
+          icon: StarHalf,
+          props: {
+            weight: 'fill',
+          },
+        }
+      }
+
+      if (isCompleted) {
+        return {
+          id: position,
+          icon: Star,
+          props: {
+            weight: 'fill',
+          },
+        }
+      }
+
+      return {
+        id: position,
+        icon: Star,
+        props: {
+          weight: 'bold',
+        },
+      }
+    },
+  )
+
+  return starts
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -48,6 +48,21 @@ module.exports = {
         'gradient-vertical': `linear-gradient(180deg, #7FD1CC 0%, #9694F5 100%)`,
         'gradient-horizontal': `linear-gradient(90deg, #7FD1CC 0%, #9694F5 100%)`,
       },
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: 0, transform: 'translateY(-2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+        slideUpAndFade: {
+          from: { opacity: 0, transform: 'translateY(2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        slideDownAndFade:
+          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        slideUpAndFade: 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
     },
   },
   plugins: [],
