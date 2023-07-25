0da5364470703f67a8662d57b4e0ab21a20fe04d
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-07-24 21:00:38 -0300

    feat: add api route to /books/bookId/ratings

---
+++ src/pages/api/books/[bookId]/ratings.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { prisma } from '@/lib/prisma'
+import { Rating } from '@/types'
+
+type Data = {
+  ratings: Rating[]
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  const isGetMethod = req.method === 'GET'
+  const { bookId } = req.query
+
+  if (isGetMethod && bookId) {
+    const ratings = await prisma.rating.findMany({
+      where: {
+        book_id: bookId as string,
+      },
+    })
+
+    return res.status(200).json({ ratings })
+  }
+
+  return res.status(404)
+}
