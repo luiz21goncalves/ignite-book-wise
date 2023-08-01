9e7c73406572a756f8149019c65cc1624c1bdaa5
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-08-01 08:11:32 -0300

    feat: add profile page

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -3,18 +3,29 @@ import Image from 'next/image'
 type AvatarProps = {
   src: string
   alt: string
+  size?: 'md' | 'lg'
+}
+
+const SIZES = {
+  md: 36,
+  lg: 72,
 }
 
 export function Avatar(props: AvatarProps) {
-  const { alt, src } = props
+  const { alt, src, size = 'md' } = props
+
+  const value = SIZES[size]
 
   return (
-    <div className="h-9 w-9 flex-shrink-0 overflow-hidden rounded-full bg-gradient-vertical object-cover p-[1px]">
+    <div
+      data-size={size}
+      className="h-9 w-9 flex-shrink-0 overflow-hidden rounded-full bg-gradient-vertical object-cover p-[1px] data-[size=lg]:h-[4.5rem] data-[size=lg]:w-[4.5rem] data-[size=lg]:p-[2px]"
+    >
       <Image
         src={src}
         alt={alt}
-        width={36}
-        height={36}
+        width={value}
+        height={value}
         className="h-full w-full rounded-full object-cover"
       />
     </div>
